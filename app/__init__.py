c75bc6757251a3ac47e55be526cb9a7c40d5029a
vivibv <vivi.barrosv.07@gmail.com>
2025-11-21 21:16:59 -0300

    arreglo init para migraciones

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate #***NUEVO: LO AGREGUÉ PARA MIGRACIONES*****
+from flask_migrate import Migrate  # NUEVO PARA MIGRACIONES
 
-# Crear primero la instancia global de la base de datos
+# Instancias globales
 db = SQLAlchemy()
-migrate = Migrate() # ***NUEVO: LO AGREGUÉ PARA MIGRACIONES*****
+migrate = Migrate()  # NUEVO PARA MIGRACIONES
+
 def create_app():
     app = Flask(__name__)
 
-    # Configuración de la base de datos: usa SQLite y crea habits.db en la carpeta del proyecto
+    # Configuración Base de datos
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///habits.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # Vincular la base de datos a la aplicación
+    # Inicializar SQLAlchemy
     db.init_app(app)
 
-    # Importar modelos (IMPORTANTÍSIMO: hacer esto después de crear db)
+    # Importar modelos (después de db.init_app)
     from .models import DailyHabit, WeeklyHabit, WeeklyHabitCompletion
 
     # Importar rutas
@@ -28,11 +29,7 @@ def create_app():
     app.register_blueprint(logs)
     app.register_blueprint(stats)
 
- # Inicializar Flask-Migrate
-    migrate.init_app(app, db)   # ***NUEVO PARA MIGRACIONES ****
-    
-    # Crear tablas dentro del contexto de la aplicación
-    with app.app_context():
-        db.create_all()
+    # Inicializar Migraciones 
+    migrate.init_app(app, db)
 
-    return app
+    return app   
